f836afde283c8a40f381cd38433b92ee248a1ea7
czp <celenzo.peuch@epitech.eu>
2026-02-09 01:28:34 +0100

    feat: LeetCode problem 55

---
+++ Array/Jump_Game_55.c
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2026
+** LeetCode
+** File description:
+** 55
+*/
+
+#include <stdbool.h>
+
+bool canJump(int* nums, int numsSize){
+    int jump = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (jump < i) {
+            break;
+        }
+        if (jump < i + nums[i]) {
+            jump = i + nums[i];
+        }
+        if (jump >= numsSize - 1) {
+            return true;
+        }
+    }
+    return false;
+}
