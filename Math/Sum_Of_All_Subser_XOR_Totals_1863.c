4de0ec1ee9127d02a28171971af1946b652ef9a2
czp <celenzo.peuch@epitech.eu>
2026-03-08 15:15:49 +0100

    feat: LeetCode problem 1863

---
+++ Math/Sum_Of_All_Subser_XOR_Totals_1863.c
@@ -0,0 +1,11 @@
+int subsetXORSum(int* nums, int numsSize) {
+    int result = 0;
+    int allXOR = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        allXOR |= nums[i];
+    }
+
+    result = allXOR * (1 << (numsSize - 1));
+    return result;   
+}
